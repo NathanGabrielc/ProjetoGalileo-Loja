69cb87968fb15e6bbb2de74c3eaf8341826f9900
Nathan Gabriel <nathangbrc@gmail.com>
2022-07-25 18:20:37 -0300

    vitrine.js

--- vitrine.js
+++ vitrine.js
@@ -1,15 +1,28 @@
-const button = document.querySelector("button")
-button.id = "submit";
-
-const submit = document.getElementById("submit");
-
-submit.addEventListener("click", validate);
-
+const form = document.querySelector("form");
 const email = document.getElementById("email");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  validacao();
+});
 
-function validate(){
-if(email == ""){
-    console.log ("certo")
+function validacao() {
+  if (email.value == "") {
+    const valor = prompt("Qual é o seu endereço de e-mail?", "");
+    if (valor.length == 0) {
+      alert("ERRO - nenhum valor informado no Prompt!");
+    }
+    if (valor.indexOf("@") == -1) {
+      alert("Não há '@' no seu valor");
+    }
+    if (valor.indexOf(".") == -1) {
+      alert("Não há '.' em seu valor");
+    }
+    if (valor.lastIndexOf(".") < valor.indexOf("@") - 1) {
+      alert("deve haver um . depois do @");
+    }
+    if (valor.length <= 9) {
+      /* Deve ter o tamanho mínimo de 10 caracteres. */
+      alert("seu email deve conter 10 caracteres!");
+    }
+  }
 }
-}
-
